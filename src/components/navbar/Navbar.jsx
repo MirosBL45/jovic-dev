d87e639fed653bdf9f9d9c3bb60faac38157fddf
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-10-27 18:31:29 +0200

    starting navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+// react/next stuff
+import Link from 'next/link';
+
+import { links } from '@/utils/data';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link href={'/'}>jovic-dev</Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,10 +1,32 @@
-export const firstText = [
+export const links = [
   {
-    headline: 'First',
-    text: 'Go smart',
+    id: 1,
+    title: "Home",
+    url: "/",
   },
   {
-    headline: 'First',
-    text: 'Go smart',
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
   },
 ];
