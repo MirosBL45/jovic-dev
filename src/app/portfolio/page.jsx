8fac633e0dad462fef1cf55207a19f8bbdb24395
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-10-31 20:13:20 +0100

    started portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,19 @@
+// react/next stuff
+// import Link from 'next/link';
+
+// data
+// import { links } from '@/utils/data';
+
+// style
 import styles from './page.module.css';
 
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h2>Choose an area</h2>
+      <div className={styles.items}></div>
+    </div>
+  );
 }
 
 export default Portfolio;
