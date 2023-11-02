25f3b5b4e926de0492ab76a58c7dd4d486c5133a
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-11-02 21:06:17 +0100

    portfolio done for now

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+// react/next stuff
+import Image from 'next/image';
 
-function Category() {
-  return <div>Category</div>;
+// components
+import Button from '@/components/Button/Button';
+
+// style
+import styles from './page.module.css';
+
+function Category({ params }) {
+  return (
+    <div>
+      <h2 className={styles.catTitle}>{params.category}</h2>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h3 className={styles.title}>Test</h3>
+          <p className={styles.description}>Desc</p>
+          <Button text={'See More'} url={'#'} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image fill={true} src={'/people-working2.jpg'} alt="jovic-dev" />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h3 className={styles.title}>Test</h3>
+          <p className={styles.description}>Desc</p>
+          <Button text={'See More'} url={'#'} />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image fill={true} src={'/people-working2.jpg'} alt="jovic-dev" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Category;
