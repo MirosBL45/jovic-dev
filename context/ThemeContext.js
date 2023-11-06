35a3294edf58cb7cfc7b90d5fc7a77a47a44a343
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-11-06 10:08:31 +0100

    dark mode done

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -14,7 +14,7 @@ export const ThemeProvider = ({ children }) => {
 
     return (
         <ThemeContext.Provider value={{ toggle, mode }}>
-            <div className={`theme ${mode}`}>{children}</div>
+            <div className={`light ${mode}`}>{children}</div>
         </ThemeContext.Provider>
     )
 }
