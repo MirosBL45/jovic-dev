f0935b27cd73f00583d15f4f3a18067b1baf7098
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-11-15 10:17:57 +0100

    started login with credentials

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -10,12 +10,35 @@ import ClickButton from '@/components/Buttons/ClickButton';
 import styles from './page.module.css';
 
 function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn('credentials', { email, password });
+  }
+
   function handleClick() {
     signIn('google');
   }
 
   return (
     <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className={styles.input}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+          required
+        />
+        <ClickButton>{'Login'}</ClickButton>
+      </form>
       <ClickButton onClick={handleClick}>{'Login with Google'}</ClickButton>
     </div>
   );
