713be9011d34ec90d6ebf5749769c09e0338a5ff
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-12-23 23:20:22 +0100

    take care of images

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -59,16 +59,12 @@ async function BlogPost({ params }) {
               </div>
             </div>
             <div className={styles.imageContainer}>
-              {/* <img loading="lazy" src={data.image} alt={data.title} /> */}
               <Image
                 src={data.image}
                 alt={data.title}
-                // width={500}
-                // height={300}
                 width={0}
                 height={0}
                 sizes="100vw"
-                style={{ width: '100%', height: '300px' }}
               />
             </div>
           </section>
