cc72315a5f4ed7a456cf9d34552ce735e7258201
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-12-24 16:07:39 +0100

    notFound done

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,55 @@
+'use client'
+
+// react/next stuff
 import Link from 'next/link'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+// style
+import styles from './page.module.css';
+import './globals.css'
+
+// images
+import notFound from 'public/notFound.svg';
 
 export default function NotFound() {
+    // Time in seconds
+    const timeout = 60;
+    const router = useRouter();
+    const [counter, setCounter] = useState(timeout);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter((prevCounter) => prevCounter - 1);
+        }, 1000);
+
+        setTimeout(() => {
+            router.push('/');
+        }, timeout * 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [router]);
+
     return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource for you. Hello by Ruby the Cat</p>
-            <Link href="/">Return Home</Link>
+        <div className={styles.notFoundContainer}>
+            <div className={styles.notFoundText}>
+                <h2>Whoops!!!</h2>
+                <p>It seems you've ventured into uncharted territory.</p>
+                <p>The page you're looking for might be on a vacation or has taken a coffee break.</p>
+                <p>In the meantime, if you're feeling adventurous, you can explore other corners of our digital realm.</p>
+                <p>If you believe this is a technical glitch, feel free to let <Link href="/contact">our support</Link> team know.</p>
+                <p>Thanks for your patience, and happy exploring!</p>
+                <p>
+                    <Link href="/">Return Home</Link> or you will be
+                    redirected in {counter} seconds.
+                </p>
+            </div>
+            <div className={styles.item}>
+                <Image src={notFound} alt="notFound" className='moveImage' />
+            </div>
         </div>
     )
 }
