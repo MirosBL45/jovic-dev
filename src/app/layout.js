8bdb3512f7a13d1ad2cbc3d9e8b36b9603395491
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-04-27 12:35:05 +0200

    meta tags working

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,9 @@ import './globals.css'
 
 // Static metadata
 export const metadata = {
-  title: 'Jović Miroslav',
-  description: 'Jović Miroslav - Frontend Developer. I build fast, stable and responsive web applications and websites. My role is to leverage sophisticated Frontend development skills to help digital visions become a reality. Focusing on React & Next.js, I use state-of-the-art technologies to breathe life into web projects. This is my Portfolio WebApp made with Next.js, React, MongoDB, Mongoose and SWR',
-  keywords: 'HTML, CSS, SCSS, Sass, JavaScript, programming, ReactJS, NextJS, Next.js, React, Vue.js, MongoDB, Mongoose, SWR, Miroslav Jovic, Frontend Developer, Web Development, Portfolio, Blog, Dashboard',
+  title: 'Jović Miroslav | Frontend Developer',
+  description: 'Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
+  keywords: 'HTML, CSS, SCSS, Sass, JavaScript, programming, ReactJS, NextJS, Next.js, React, Vue.js, MongoDB, Mongoose, SWR, Miroslav Jovic, Jović Miroslav, Frontend Developer, Web Development, Portfolio, Blog, Dashboard, Frontend Design, User Interface (UI), User Experience (UX), Responsive Design, Web Applications, Single Page Applications (SPA), Component-Based Development, Version Control (Git), API Integration, Progressive Web Apps (PWA), Performance Optimization, Frontend Frameworks, Frontend Libraries, Frontend Tools, Web Development Tools, Cross-Browser Compatibility, Mobile-First Development, Accessibility (A11y), SEO Optimization, Performance Monitoring, Debugging Techniques, Code Refactoring, Agile Development, Scrum Methodology, Git, GitHub, TailwindCSS, ViteJS, ThreeJS, Framer Motion, React Icons, ReactStrap, MaterialUI, Styled-components, Bootstrap, TypeScript, Firebase, Hygraph (GraphQL), Bcryptjs, Postman, Strapi, NextAuth, Figma, Photoshop, Premiere pro, Modern JavaScript Frameworks, UI/UX Design Principles, CSS Frameworks, CSS Preprocessors, Frontend Build Tools, Frontend Testing, Browser Developer Tools, Responsive Web Design, Animation Effects, Code Optimization Techniques, Web Performance Metrics, Continuous Integration (CI), Deployment Strategies, Frontend Architecture, State Management,  Functional Programming in JavaScript, Asynchronous JavaScript (Promises, Async/Await), Frontend Security Best Practices, Web Accessibility Standards, Browser Compatibility Testing, Frontend Performance Optimization Techniques, Code Versioning, WebSockets, Server-Side Rendering (SSR), Static Site Generation (SSG), Progressive Enhancement, Internationalization (i18n) and Localization',
   icons: {
     icon: [{ url: '/small logo Miroslav Jovic Frontend Developer.png' }],
     shortcut: '/small logo Miroslav Jovic Frontend Developer.png',
@@ -36,14 +36,14 @@ export const metadata = {
   applicationName: 'Jović Miroslav',
   authors: [{ name: 'Jović Miroslav' }],
   twitter: {
-    title: 'Jović Miroslav',
-    description: 'Jović Miroslav - Frontend Developer. I build fast, stable and responsive web applications and websites. My role is to leverage sophisticated Frontend development skills to help digital visions become a reality. Focusing on React & Next.js, I use state-of-the-art technologies to breathe life into web projects. This is my Portfolio WebApp made with Next.js, React, MongoDB, Mongoose and SWR',
+    title: 'Jović Miroslav | Frontend Developer',
+    description: 'Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
     creator: 'Jović Miroslav',
   },
   openGraph: {
     images: '/small logo Miroslav Jovic Frontend Developer.png',
-    title: 'Jović Miroslav',
-    description: 'Jović Miroslav - Frontend Developer. I build fast, stable and responsive web applications and websites. My role is to leverage sophisticated Frontend development skills to help digital visions become a reality. Focusing on React & Next.js, I use state-of-the-art technologies to breathe life into web projects. This is my Portfolio WebApp made with Next.js, React, MongoDB, Mongoose and SWR',
+    title: 'Jović Miroslav | Frontend Developer',
+    description: 'Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
     siteName: 'Jović Miroslav',
     type: 'website',
     authors: ['Jović Miroslav'],
