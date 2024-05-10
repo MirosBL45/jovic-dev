0d1bc2f33302b4abc78c34e1694a5e2ef35140ee
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-05-10 23:21:56 +0200

    icon type added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,19 @@ export const metadata = {
   description: 'Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
   keywords: 'HTML, CSS, SCSS, Sass, JavaScript, programming, ReactJS, NextJS, Next.js, React, Vue.js, MongoDB, Mongoose, SWR, Miroslav Jovic, Miroslav Jović, Frontend Developer, Web Development, Portfolio, Blog, Dashboard, Frontend Design, User Interface (UI), User Experience (UX), Responsive Design, Web Applications, Single Page Applications (SPA), Component-Based Development, Version Control (Git), API Integration, Progressive Web Apps (PWA), Performance Optimization, Frontend Frameworks, Frontend Libraries, Frontend Tools, Web Development Tools, Cross-Browser Compatibility, Mobile-First Development, Accessibility (A11y), SEO Optimization, Performance Monitoring, Debugging Techniques, Code Refactoring, Agile Development, Scrum Methodology, Git, GitHub, TailwindCSS, ViteJS, ThreeJS, Framer Motion, React Icons, ReactStrap, MaterialUI, Styled-components, Bootstrap, TypeScript, Firebase, Hygraph (GraphQL), Bcryptjs, Postman, Strapi, NextAuth, Figma, Photoshop, Premiere pro, Modern JavaScript Frameworks, UI/UX Design Principles, CSS Frameworks, CSS Preprocessors, Frontend Build Tools, Frontend Testing, Browser Developer Tools, Responsive Web Design, Animation Effects, Code Optimization Techniques, Web Performance Metrics, Continuous Integration (CI), Deployment Strategies, Frontend Architecture, State Management,  Functional Programming in JavaScript, Asynchronous JavaScript (Promises, Async/Await), Frontend Security Best Practices, Web Accessibility Standards, Browser Compatibility Testing, Frontend Performance Optimization Techniques, Code Versioning, WebSockets, Server-Side Rendering (SSR), Static Site Generation (SSG), Progressive Enhancement, Internationalization (i18n) and Localization',
   icons: {
-    icon: [{ url: '/small logo Miroslav Jovic Frontend Developer.png' }],
-    shortcut: '/small logo Miroslav Jovic Frontend Developer.png',
-    apple: '/small logo Miroslav Jovic Frontend Developer.png',
+    icon: [{ url: '/small logo Miroslav Jovic Frontend Developer.png', type: 'image/png' }],
+    shortcut: {
+      url: '/small logo Miroslav Jovic Frontend Developer.png',
+      type: "image/png"
+    },
+    apple: {
+      url: '/small logo Miroslav Jovic Frontend Developer.png',
+      type: "image/png"
+    },
     other: {
       rel: 'apple-touch-icon-precomposed',
       url: '/small logo Miroslav Jovic Frontend Developer.png',
+      type: "image/png"
     },
   },
   robots: {
