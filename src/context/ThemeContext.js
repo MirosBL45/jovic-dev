12d848bdca47097acc7d46db011ea9817903239f
MirosBL45 <jovicmiroslav88@gmail.com>
2025-04-06 16:12:02 +0200

    CV updated and theme in LocalStorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,16 +1,41 @@
 'use client'
 
 // react/next stuff
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [mode, setMode] = useState('light');
+    // initially undefined
+    const [mode, setMode] = useState(undefined);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // check the theme in the localStorage
+            const stored = localStorage.getItem('theme');
+            if (stored) {
+                setMode(stored);
+            } else {
+                // check prefers-color-scheme of system
+                const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                setMode(prefersDark ? 'dark' : 'light');
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (mode) {
+            // set the theme in the localStorage
+            localStorage.setItem('theme', mode);
+        }
+    }, [mode]);
 
     function toggle() {
         setMode(prev => prev === 'dark' ? 'light' : 'dark');
     }
+
+    // Until we know what the theme is, don't render anything
+    if (!mode) return null;
 
     return (
         <ThemeContext.Provider value={{ toggle, mode }}>
