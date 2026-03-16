95954aa5727948c4c68703806681966e0656fc76
MirosBL45 <jovicmiroslav88@gmail.com>
2026-03-16 15:43:43 +0100

    started with terms page

---
+++ src/app/terms/page.jsx
@@ -0,0 +1,32 @@
+// Static metadata
+export const metadata = {
+  title: 'Terms Jović Miroslav | Frontend Developer',
+  description:
+    'Contact Jović Miroslav on the web. Send me a message, I look forward to our meeting. Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
+  twitter: {
+    title: 'Terms Jović Miroslav | Frontend Developer',
+    description:
+      'Contact Jović Miroslav on the web. Send me a message, I look forward to our meeting. Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
+  },
+  openGraph: {
+    title: 'Terms Jović Miroslav | Frontend Developer',
+    description:
+      'Contact Jović Miroslav on the web. Send me a message, I look forward to our meeting. Explore my portfolio showcasing quick, stable, and responsive online applications and websites crafted with Next.js, React, MongoDB, Mongoose, and SWR. As a frontend developer, I bring digital dreams to life with cutting-edge technologies, ensuring seamless user experiences. Dive into my work and discover the power of innovative frontend development',
+  },
+};
+
+function Terms() {
+  return (
+    <main>
+      <header>
+        {/* <h1>{contactData.headlineText}</h1> */}
+        <h1>Terms of Service</h1>
+        
+      </header>
+      <p>Welcome to our Terms of Service!</p>
+      <p>text</p>
+    </main>
+  );
+}
+
+export default Terms;
