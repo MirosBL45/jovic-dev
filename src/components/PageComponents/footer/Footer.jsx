10801fcb1bb1b9d3f4d4730242dc3e3dbdeeb85c
MirosBL45 <jovicmiroslav88@gmail.com>
2026-03-17 20:32:29 +0100

    finished terms and privacy

--- src/components/PageComponents/footer/Footer.jsx
+++ src/components/PageComponents/footer/Footer.jsx
@@ -1,11 +1,11 @@
 // react/next stuff
-import Link from 'next/link';
+import Link from "next/link";
 
 // style
-import styles from './footer.module.css';
+import styles from "./footer.module.css";
 
 // icons
-import { FaLinkedin, FaGithub, FaXTwitter } from 'react-icons/fa6';
+import { FaLinkedin, FaGithub, FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
@@ -14,50 +14,67 @@ function Footer() {
     <footer className={styles.container}>
       <div className={styles.links}>
         <span>
-          &copy; {currentYear} Designed by{' '}
+          &copy; {currentYear} Designed by{" "}
           <Link
             rel="noopener noreferrer"
             target="_blank"
-            href={'https://www.devu.design/'}
-            title={'Design & Dev Studio – where Creativity & Technology unite.'}
+            href={"https://www.devu.design/"}
+            title={"Design & Dev Studio – where Creativity & Technology unite."}
           >
             Devu Design
           </Link>
           .
-        </span>{' '}
+        </span>{" "}
         <span>
-          Developed by{' '}
-          <Link title={'Miroslav Jovic Frontend Developer'} href={'/contact'}>
+          Developed by{" "}
+          <Link title={"Miroslav Jovic Frontend Developer"} href={"/contact"}>
             Miroslav Jovic
           </Link>
           .
         </span>
       </div>
-      <div className={styles.icons}>
-        <Link
-          rel="noopener noreferrer"
-          target="_blank"
-          href={'https://www.linkedin.com/in/mj888/'}
-          title={`Jovic Miroslav's Frontend Developer Linkedin profile`}
-        >
-          <FaLinkedin />
-        </Link>
-        <Link
-          rel="noopener noreferrer"
-          target="_blank"
-          href={'https://github.com/MirosBL45'}
-          title={`Jovic Miroslav's Frontend Developer Github profile`}
-        >
-          <FaGithub />
-        </Link>
-        <Link
-          rel="noopener noreferrer"
-          target="_blank"
-          href={'https://x.com/MiroslavJovic45'}
-          title={`Jovic Miroslav's Frontend Developer X profile`}
-        >
-          <FaXTwitter />
-        </Link>
+      <div className={styles.termsAndIcons}>
+        <div className={styles.icons}>
+          <Link
+            rel="noopener noreferrer"
+            target="_blank"
+            href={"https://www.linkedin.com/in/mj888/"}
+            title={`Jovic Miroslav's Frontend Developer Linkedin profile`}
+          >
+            <FaLinkedin />
+          </Link>
+          <Link
+            rel="noopener noreferrer"
+            target="_blank"
+            href={"https://github.com/MirosBL45"}
+            title={`Jovic Miroslav's Frontend Developer Github profile`}
+          >
+            <FaGithub />
+          </Link>
+          <Link
+            rel="noopener noreferrer"
+            target="_blank"
+            href={"https://x.com/MiroslavJovic45"}
+            title={`Jovic Miroslav's Frontend Developer X profile`}
+          >
+            <FaXTwitter />
+          </Link>
+        </div>
+        <div className={styles.terms}>
+          <Link
+            href="/terms"
+            title={`Jovic Miroslav's Frontend Developer Terms of Service`}
+          >
+            Terms of Service
+          </Link>
+          |
+          <Link
+            href="/privacy"
+            title={`Jovic Miroslav's Frontend Developer Privacy Policy`}
+          >
+            Privacy Policy
+          </Link>
+        </div>
       </div>
     </footer>
   );
